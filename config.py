ffb15b269ba6a69ee0de573b3ceba98e9f8afb73
ShaneH <shane.hollaway@gmail.com>
2017-11-27 20:16:53 +1000

    Add support for SunnyBoy 5 Inverter

--- config.py
+++ config.py
@@ -1,1 +1,18 @@
 # See config-example.py for required and optional settings.
+
+inverter_ip = "192.168.1.120"
+inverter_port = 502
+slave = 3
+timeout = 3
+scan_interval = 10
+model = "sma-SBn_n-1AV-40"
+# Optional:
+#dweepy_uuid = "random-uuid"
+# Optional:
+#influxdb_ip = "192.168.1.128"
+#influxdb_port = 8086
+#influxdb_user = "user"
+#influxdb_password = "password"
+#influxdb_database = "inverter"
+#influxdb_ssl = True
+#influxdb_verify_ssl = False
