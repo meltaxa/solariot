41970df5527204bea30dbb50d9e499e5ce3b031c
Shawn King <slackerlinux85@gmail.com>
2021-05-28 16:17:56 +1000

    Initial Sungrow SG3K-S Modbus map
    almost identical to SG5K-D map except total power yield has been fixed and PV2 stats removed(inverter is single string not dual)

---
+++ modbus-sungrow-sg3ks.py
@@ -0,0 +1,49 @@
+read_register = {
+  "5003": "daily_power_yield_0.01", # Wh
+  "5004": "total_power_yield_1000", # MWh
+  "5008": "internal_temp_10",       # C
+  "5011": "pv1_voltage_10",         # V
+  "5012": "pv1_current_10",         # A
+  "5017": "total_pv_power",         # W
+  "5019": "grid_voltage_10",        # V
+  "5022": "inverter_current_10",    # A
+  "5031": "total_active_power",     # W
+  "5036": "grid_frequency_10",      # Hz
+}
+
+holding_register = {
+  "5000": "year",
+  "5001": "month",
+  "5002": "day",
+  "5003": "hour",
+  "5004": "minute",
+  "5005": "second"
+}
+
+scan = """{
+    "read": [
+        {
+            "start": "5000",
+            "range": "100"
+        },
+        {
+            "start": "5100",
+            "range": "50"
+        }
+  ],
+  "holding": [
+    {
+      "start": "4999",
+      "range": "10"
+    }
+  ]
+}"""
+
+# Match Modbus registers to pvoutput api fields
+# Reference: https://pvoutput.org/help.html#api-addstatus
+pvoutput = {
+  "Energy Generation": "daily_power_yield",
+  "Power Generation": "total_active_power",
+  "Temperature": "internal_temp",
+  "Voltage": "grid_voltage"
+}
